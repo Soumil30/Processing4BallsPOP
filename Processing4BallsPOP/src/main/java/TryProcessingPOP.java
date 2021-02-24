59b971cf97938550acd8936f3ea413338e0874ee
Soumil De <79584210+Soumil30@users.noreply.github.com>
2021-02-24 22:16:42 +0530

    Add files via upload

---
+++ Processing4BallsPOP/src/main/java/TryProcessingPOP.java
@@ -0,0 +1,38 @@
+import processing.core.PApplet;
+
+public class TryProcessingPOP extends PApplet {
+
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    public static final int DIAMETER = 20;
+    public static final float FRACTION = 0.2f;
+    private int x;
+
+    public static void main(String[] args) {
+        PApplet.main("TryProcessingPOP", args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+        x = 0;
+    }
+
+    @Override
+    public void draw() {
+        for (int ball = 1; ball <= 4; ball++) {
+            drawCircle(ball);
+        }
+        x++;
+    }
+
+    private void drawCircle(int ball) {
+        ellipse(ball * x, HEIGHT * ball * FRACTION, DIAMETER, DIAMETER);
+    }
+
+}
